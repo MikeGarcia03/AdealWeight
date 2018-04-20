9cdf7ed89ef30c1d467ed59cff7b3aab344cb171
MikeGarcia03 <miguelangelgarciadesena@gmail.com>
2018-04-20 18:07:32 -0400

    Diseño del area de calculo de datos terminado.

--- feature/src/main/java/com/example/tics/adealwight/feature/MainActivity.java
+++ feature/src/main/java/com/example/tics/adealwight/feature/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.tics.adealwight.feature;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ideal_weight_calculator);
+        setContentView(R.layout.activity_main);
+
+        ImageButton start = findViewById(R.id.btnStart);
+        
     }
 }
